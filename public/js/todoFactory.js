bc6543105240316edb68a6633bf1c12e2d00f4db
Courtney McRae <courtneymcrae9@gmail.com>
2016-03-16 14:10:50 -0700

    Adds todoFactory

---
+++ public/js/todoFactory.js
@@ -0,0 +1,31 @@
+angular.module('todosApp')
+	.factory('todosFactory', todosFactory)
+
+todosFactory.$inject = ['$http']
+
+function todosFactory($http){
+	var todosUrl = 'http://localhost:3000/api/todos'
+	var todos = {}
+
+	todos.list = function(){
+		return $http.get(todosUrl)
+	}
+
+	todos.show = function(todoId){
+		return $http.get(todosUrl + '/' + todoId)
+	}
+
+	todos.addTodo = function(data){
+		return $http.post(todosUrl, data)
+	}
+
+	todos.updateTodo = function(todoId,data){
+		return $http.patch(todosUrl + '/' + todoId, data)
+	}
+
+	todos.removeTodo = function(todoId){
+		return $http.delete(todosUrl + '/' + todoId)
+	}
+	
+	return todos
+}
