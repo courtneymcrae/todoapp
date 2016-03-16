3f3619a4521abdcfdf2610969b5db260ed01bf43
Courtney McRae <courtneymcrae9@gmail.com>
2016-03-16 14:08:51 -0700

    Creates a public folder and an app.js file

---
+++ public/js/app.js
@@ -0,0 +1,27 @@
+angular.module('todosApp', ['ui.router'])
+	.directive('navBar', navBar)
+	.directive('todoForm', todoForm)
+	.filter('reverse', reverse)
+
+function reverse() {
+	return function(items) {
+		return items.slice().reverse();
+	};
+}
+
+function carForm(){
+	var directive = {
+		restrict: 'E',
+		templateUrl: '/partials/todo-form.html'
+	}
+	return directive
+}
+
+function navBar(){
+	var directive = {
+		restrict: 'E',
+		templateUrl: '/partials/nav.html',
+		transclude: true
+	}
+	return directive
+}
