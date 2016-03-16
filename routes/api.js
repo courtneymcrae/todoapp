147cc4169adf97f194699d2912a551ba1b9711f1
Courtney McRae <courtneymcrae9@gmail.com>
2016-03-16 14:37:53 -0700

    Adds api routes

---
+++ routes/api.js
@@ -0,0 +1,21 @@
+var
+	express = require('express'),
+	apiRouter = express.Router(),
+	mongoose = require('mongoose'),
+	todosController = require('../controllers/todosController.js');
+	
+
+apiRouter.get('/', function(req,res){
+	res.json({message: "Api routes are working."})
+})
+
+apiRouter.route('/todos')
+	.get(todosController.getAlltodos)
+	.post(todosController.createtodo)
+
+apiRouter.route('/todos/:id')
+	.get(todosController.getOneTodo)
+	.patch(todosController.updateTodo)
+	.delete(todosController.deleteTodo)
+
+module.exports = apiRouter
