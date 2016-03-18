fed051b3e6029631566845412cd8c41b5c563566
Courtney McRae <courtneymcrae9@gmail.com>
2016-03-17 20:31:57 -0700

    Updates todosController

---
+++ public/js/todoDetailsController.js
@@ -0,0 +1,35 @@
+angular.module('reviewApp')
+	.controller('TodoDetailsController', TodoDetailsController)
+
+TodoDetailsController.$inject = ['todosFactory','$stateParams','$location']
+
+function TodoDetailsController(todosFactory,$stateParams,$location){
+	var vm = this
+	vm.name = 'Todo Detail'
+	vm.api = todosFactory
+	vm.todo = null
+	vm.editing = false
+	vm.showtodo = function(todoId){
+		vm.api.show(todoId).success(function(response){
+			vm.todo = response
+			console.log(response)
+		})
+	}
+	vm.showtodo($stateParams.todoId)
+
+	vm.updatetodo = function(todoId, name, completed){
+		var data = {name: name, completed: completed}
+		vm.api.updateTodo(todoId,data).success(function(response){
+			console.log(response)
+			vm.todo = response
+			vm.editing = false
+		})
+	}
+
+	vm.removeTodo = function(TodoId){
+		vm.api.removeTodo(todoId).success(function(response){
+			console.log(response)
+			$location.path('/todos')
+		})
+	}
+}
