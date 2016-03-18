15a020be451ce125f1ed570415ba63f7a616dbe9
Courtney McRae <courtneymcrae9@gmail.com>
2016-03-18 13:48:43 -0700

    Adds goal model

--- api/models/Goals.js
+++ api/models/Goals.js
@@ -2,11 +2,11 @@ var
 	mongoose = require('mongoose'),
 	Schema = mongoose.Schema
 
-var todoSchema = new Schema({
+var goalsSchema = new Schema({
 	name: String,
     // completed: Boolean
 })
 
-var Todo = mongoose.model('Todo', todoSchema)
+var Goal = mongoose.model('Goal', goalSchema)
 
-module.exports = Todo
+module.exports = Goal
