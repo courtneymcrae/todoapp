beb07273fbdc4e42345cb87bf68945925d5aa851
Courtney McRae <courtneymcrae9@gmail.com>
2016-03-17 20:23:54 -0700

    Updates router

---
+++ public/js/router.js
@@ -0,0 +1,43 @@
+angular.module('reviewApp')
+	.config(MainRouter)
+	.config(interceptor)
+
+function interceptor($httpProvider) {
+	$httpProvider.interceptors.push('authInterceptorFactory')
+}
+
+function MainRouter($stateProvider, $urlRouterProvider) {
+	$urlRouterProvider.otherwise('/login')
+
+	$stateProvider
+		.state('home', {
+			url: '/home',
+			templateUrl: 'partials/home.html',
+			controller: 'UsersController as usersCtrl'
+		})
+		.state('login', {
+			url: '/login',
+			templateUrl: 'partials/login.html',
+			controller: 'UsersController as usersCtrl'
+		})
+		.state('signup', {
+			url: '/signup',
+			templateUrl: 'partials/signup.html',
+			controller: 'UsersController as usersCtrl'
+		})
+		.state('loggedOut', {
+			url: '/loggedOut',
+			templateUrl: 'partials/home.html',
+			controller: 'UsersController as usersCtrl'
+		})
+		.state('todos', {
+			url: '/todos',
+			templateUrl: 'partials/todo-list.html',
+			controller: 'todosController as todosCtrl'
+		})
+		.state('detail', {
+			url: '/todos/:todoId',
+			templateUrl: 'partials/todo-detail.html',
+			controller: 'todoDetailsController as todoDetailsCtrl'
+		})
+}
